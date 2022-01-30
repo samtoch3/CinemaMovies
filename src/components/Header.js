1956c23ea6c1153e41952ca81c2b339d497b15a2
Samson N <samson.nzumgueng@outlook.com>
2022-01-30 12:55:31 +0100

    first commit : initialisation

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return ( <
+        div className = 'header' >
+        <
+        nav >
+        <
+        ul >
+        <
+        li > Accueil < /li> <
+        li > Coup de coeur < /li> < /
+        ul > <
+        /nav> < /
+        div >
+    );
+};
+
+export default Header;
